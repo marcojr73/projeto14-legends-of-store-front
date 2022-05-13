eb4da25bceeb6daa221fbce738f13fab1146c4cc
marco junior <marcojuniorengineer@gmail.com>
2022-05-13 09:34:01 -0300

    style bag

--- src/components/HomeRight.js
+++ src/components/HomeRight.js
@@ -4,6 +4,7 @@ import ContainerLogin from './ContainerLogin';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
 import styled from "styled-components";
+import tresh from "../assets/images/tresh.png"
 
 export default function HomeRight(){
     return(
@@ -29,7 +30,23 @@ export default function HomeRight(){
 
             </div>
             <Cart>
-
+                <div className="profile">
+                    <div className="buy">
+                        <img src={tresh} />
+                        <p>.descrição</p>
+                        <button className="purchase">Finalizar Compra</button>
+                    </div>
+                    <div className="bag">
+                        <div className="square" > <img src={tresh} /> </div>
+                        <div className="square" > <img src={tresh} /> </div>
+                        <div className="square" > <img src={tresh} /> </div>
+                        <div className="square" ></div>
+                        <div className="square" ></div>
+                        <div className="square" ></div>
+                        <div className="square" ></div>
+                        <div className="square" ></div>
+                    </div>
+                </div>
             </Cart>
         </PurchaseDetails>
     )
@@ -90,4 +107,53 @@ const Showcase = styled.div`
 
 const Cart = styled.div`
     height: 50vh;
+    display: flex;
+    justify-content: center;
+    margin-top: 60px;
+
+    .profile{
+        width: 886px;
+        height: 350px;
+        background: #102028;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+
+    .buy{
+        height: 250px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .purchase{
+        background: #396A82;
+        border-radius: 6px;
+        border: none;
+        width: 150px;
+        height: 45px;
+    }
+
+    .bag{
+        width: 586px;
+        height: 250px;
+        background: #13262F;
+        display: grid;
+        grid-template-rows: 1fr 1fr;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    }
+
+    .square{
+        margin-top: 15px;
+        margin-left: 10px;
+        width: 100px;
+        height: 100px;
+        background: #102028;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
 `
